58304c320a5f062a8757595956d51b44218af360
Lucas Moura <lucas.l.moura@grupoboticario.com.br>
2021-08-24 17:19:54 -0300

    add drag

--- src/components/PDFReader/PDFReader.component.tsx
+++ src/components/PDFReader/PDFReader.component.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 import pdf from './base64.json';
+import ScrollContainer from 'react-indiana-drag-scroll'
+import './pdf.css';
 
 type PDFReaderProps = {
   file?: String;
@@ -8,15 +10,26 @@ type PDFReaderProps = {
 
 export default function PDFReader({ file }: PDFReaderProps) {
   console.log(pdf.dados)
+  const [scale, setScale] = React.useState(1);
+  
+  const zoomOut = () => {
+    setScale( state => state - 0.25)
+  }
+
   return (
     <div>
+      <button style={{padding: 15}} onClick={ zoomOut}>-</button>
+        <button style={{padding: 15}} onClick={ () => setScale( state => state + 0.25)}>+</button>
+        <p>{scale}</p>
       <h1>Leitor de PDF</h1>
-      <Document
-        file={`data:application/pdf;base64,${pdf.dados}`}
-        // onLoadSuccess={onDocumentLoadSuccess}
-      >
-        <Page pageNumber={1} />
-      </Document>
+      <ScrollContainer className="scroll-container">
+        <Document
+          file={`data:application/pdf;base64,${pdf.dados}`}
+          // onLoadSuccess={onDocumentLoadSuccess}
+          >
+          <Page pageNumber={1} scale={scale} />
+        </Document>
+        </ScrollContainer>
     </div>
   )
 }
